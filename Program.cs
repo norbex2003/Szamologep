30e2469e811ac6646d9c361abcb6e1ca0b52aa8e
Norbex2003 <105550945+norbex2003@users.noreply.github.com>
2024-05-24 15:34:33 +0200

    New repo. First 1.0 builds

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Szamologep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("As of version 2.0, there will be two separate apps for the English and Hungarian console calculator apps.");
+            Console.WriteLine("2.0 verzió esetében angol nyelv eltávolításra került! Később másik app lesz az angol nyelv");
+            Console.WriteLine("Írj be egy számot!");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Írj be még egy számot!");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double osszeadas = Math.Round(a + b, 4);
+            double kivonas = Math.Round(a - b, 4);
+            double szorzas = Math.Round(a * b, 4);
+            double osztas = Math.Round(a / b, 4);
+
+            //Változókkal való müveletek eredményének a kiiratása!
+            Console.WriteLine($"Ez az A és B változó összeadásának eredménye: {osszeadas}");
+            Console.WriteLine($"Ez az A és B változó kivonásának eredménye: {kivonas}");
+            Console.WriteLine($"Ez az A és B változó szorzásának eredménye: {szorzas}");
+            Console.WriteLine($"Ez az A és B változó szorzásának eredménye: {osztas}");
+            Console.ReadKey();
+        }
+    }
+}
