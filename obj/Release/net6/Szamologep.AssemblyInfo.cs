51a684cc7412d6d0576e38bfde19a1ab0f6e9921
Norbex2003 <norbi.cserpnyak@outlook.hu>
2024-05-28 17:40:52 +0200

    code revision

--- obj/Release/net6/Szamologep.AssemblyInfo.cs
+++ obj/Release/net6/Szamologep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Szamologep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386bae7473e850dde066e8334868eae42c1df4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc4e7d0994fab031243d0b48f829d635f9836fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Szamologep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Szamologep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
